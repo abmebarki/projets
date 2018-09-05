7f0cc0f704b056f46126e31e524220a12e095018
abmebarki <abd.mebarki@gmail.com>
2018-09-05 11:52:09 +0200

    - Corrections erreurs dans : MPD et Script tables

--- projet-3/SQL/3_db_escalade_tables.sql
+++ projet-3/SQL/3_db_escalade_tables.sql
@@ -1,15 +1,15 @@
 
-CREATE SEQUENCE public.grimpeur_id_seq_1;
+CREATE SEQUENCE public.grimpeur_id_seq;
 
 CREATE TABLE public.grimpeur (
-                id INTEGER NOT NULL DEFAULT nextval('public.grimpeur_id_seq_1'),
+                id INTEGER NOT NULL DEFAULT nextval('public.grimpeur_id_seq'),
                 nom VARCHAR(40) NOT NULL,
                 email VARCHAR(100) NOT NULL,
                 CONSTRAINT grimpeur_pk PRIMARY KEY (id)
 );
 
 
-ALTER SEQUENCE public.grimpeur_id_seq_1 OWNED BY public.grimpeur.id;
+ALTER SEQUENCE public.grimpeur_id_seq OWNED BY public.grimpeur.id;
 
 CREATE SEQUENCE public.topo_id_seq;
 
@@ -19,6 +19,7 @@ CREATE TABLE public.topo (
                 nb_pages INTEGER NOT NULL,
                 auteur VARCHAR(100) NOT NULL,
                 date DATE NOT NULL,
+                createur_id INTEGER NOT NULL,
                 CONSTRAINT topo_pk PRIMARY KEY (id)
 );
 
@@ -26,40 +27,33 @@ CREATE TABLE public.topo (
 ALTER SEQUENCE public.topo_id_seq OWNED BY public.topo.id;
 
 CREATE TABLE public.grimpeur_topo_proprietaire (
-                grimpeur_id INTEGER NOT NULL,
+                proprietaire_id INTEGER NOT NULL,
                 topo_id INTEGER NOT NULL,
-                CONSTRAINT grimpeur_topo_proprietaire_pk PRIMARY KEY (grimpeur_id, topo_id)
+                CONSTRAINT grimpeur_topo_proprietaire_pk PRIMARY KEY (proprietaire_id, topo_id)
 );
 
 
-CREATE TABLE public.grimpeur_topo_createur (
-                grimpeur_id INTEGER NOT NULL,
-                topo_id INTEGER NOT NULL,
-                CONSTRAINT grimpeur_topo_createur_pk PRIMARY KEY (grimpeur_id, topo_id)
-);
-
-
-CREATE SEQUENCE public.commentaire_site_id_seq_2;
+CREATE SEQUENCE public.commentaire_topo_id_seq;
 
 CREATE TABLE public.commentaire_topo (
-                id INTEGER NOT NULL DEFAULT nextval('public.commentaire_site_id_seq_2'),
+                id INTEGER NOT NULL DEFAULT nextval('public.commentaire_topo_id_seq'),
                 objet VARCHAR(100) NOT NULL,
                 contenu VARCHAR(1000) NOT NULL,
                 date DATE NOT NULL,
-                grimpeur_id INTEGER NOT NULL,
-                topo_id INTEGER NOT NULL,
+                auteur_id INTEGER NOT NULL,
+                topo_commente_id INTEGER NOT NULL,
                 CONSTRAINT commentaire_topo_pk PRIMARY KEY (id)
 );
 
 
-ALTER SEQUENCE public.commentaire_site_id_seq_2 OWNED BY public.commentaire_topo.id;
+ALTER SEQUENCE public.commentaire_topo_id_seq OWNED BY public.commentaire_topo.id;
 
 CREATE TABLE public.pret (
                 topo_id INTEGER NOT NULL,
-                grimpeur_id INTEGER NOT NULL,
+                emprunteur_id INTEGER NOT NULL,
                 date_debut DATE NOT NULL,
                 date_fin DATE NOT NULL,
-                CONSTRAINT pret_pk PRIMARY KEY (topo_id, grimpeur_id)
+                CONSTRAINT pret_pk PRIMARY KEY (topo_id, emprunteur_id)
 );
 
 
@@ -83,18 +77,12 @@ CREATE TABLE public.site (
                 description VARCHAR(1000) NOT NULL,
                 nb_secteurs INTEGER NOT NULL,
                 ville VARCHAR(150) NOT NULL,
+                createur_id INTEGER NOT NULL,
                 CONSTRAINT site_pk PRIMARY KEY (id)
 );
 
 
 ALTER SEQUENCE public.site_id_seq OWNED BY public.site.id;
-
-CREATE TABLE public.grimpeur_site_createur (
-                site_id INTEGER NOT NULL,
-                grimpeur_id INTEGER NOT NULL,
-                CONSTRAINT grimpeur_site_createur_pk PRIMARY KEY (site_id, grimpeur_id)
-);
-
 
 CREATE SEQUENCE public.commentaire_site_id_seq;
 
@@ -103,18 +91,18 @@ CREATE TABLE public.commentaire_site (
                 objet VARCHAR(100) NOT NULL,
                 contenu VARCHAR(1000) NOT NULL,
                 date DATE NOT NULL,
-                grimpeur_id INTEGER NOT NULL,
-                site_id INTEGER NOT NULL,
+                auteur_id INTEGER NOT NULL,
+                site_commente_id INTEGER NOT NULL,
                 CONSTRAINT commentaire_site_pk PRIMARY KEY (id)
 );
 
 
 ALTER SEQUENCE public.commentaire_site_id_seq OWNED BY public.commentaire_site.id;
 
-CREATE TABLE public.topo_site_descripteur (
+CREATE TABLE public.topo_site_descipteur (
                 site_id INTEGER NOT NULL,
                 topo_id INTEGER NOT NULL,
-                CONSTRAINT topo_site_descripteur_pk PRIMARY KEY (site_id, topo_id)
+                CONSTRAINT topo_site_descipteur_pk PRIMARY KEY (site_id, topo_id)
 );
 
 
@@ -128,7 +116,7 @@ CREATE TABLE public.secteur (
                 orientation VARCHAR(30) NOT NULL,
                 coordonnees VARCHAR(150) NOT NULL,
                 hauteur_max INTEGER NOT NULL,
-                id_1 INTEGER NOT NULL,
+                site_id INTEGER NOT NULL,
                 CONSTRAINT secteur_pk PRIMARY KEY (id)
 );
 
@@ -164,48 +152,48 @@ CREATE TABLE public.longueur (
 ALTER SEQUENCE public.longueur_id_seq OWNED BY public.longueur.id;
 
 ALTER TABLE public.pret ADD CONSTRAINT grimpeur_pret_fk
-FOREIGN KEY (grimpeur_id)
+FOREIGN KEY (emprunteur_id)
 REFERENCES public.grimpeur (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 ALTER TABLE public.commentaire_site ADD CONSTRAINT grimpeur_commentaire_fk
-FOREIGN KEY (grimpeur_id)
+FOREIGN KEY (auteur_id)
 REFERENCES public.grimpeur (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 ALTER TABLE public.commentaire_topo ADD CONSTRAINT grimpeur_commentaire_topo_fk
-FOREIGN KEY (grimpeur_id)
-REFERENCES public.grimpeur (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
-ALTER TABLE public.grimpeur_topo_createur ADD CONSTRAINT grimpeur_grimpeur_topo_fk
-FOREIGN KEY (grimpeur_id)
-REFERENCES public.grimpeur (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
-ALTER TABLE public.grimpeur_site_createur ADD CONSTRAINT grimpeur_grimpeur_site_ajout_fk
-FOREIGN KEY (grimpeur_id)
+FOREIGN KEY (auteur_id)
 REFERENCES public.grimpeur (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 ALTER TABLE public.grimpeur_topo_proprietaire ADD CONSTRAINT grimpeur_grimpeur_topo_possession_fk
-FOREIGN KEY (grimpeur_id)
+FOREIGN KEY (proprietaire_id)
 REFERENCES public.grimpeur (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.topo_site_descripteur ADD CONSTRAINT topo_topo_site_fk
+ALTER TABLE public.site ADD CONSTRAINT grimpeur_site_fk
+FOREIGN KEY (createur_id)
+REFERENCES public.grimpeur (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.topo ADD CONSTRAINT grimpeur_topo_fk
+FOREIGN KEY (createur_id)
+REFERENCES public.grimpeur (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.topo_site_descipteur ADD CONSTRAINT topo_topo_site_fk
 FOREIGN KEY (topo_id)
 REFERENCES public.topo (id)
 ON DELETE NO ACTION
@@ -220,14 +208,7 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 ALTER TABLE public.commentaire_topo ADD CONSTRAINT topo_commentaire_topo_fk
-FOREIGN KEY (topo_id)
-REFERENCES public.topo (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
-ALTER TABLE public.grimpeur_topo_createur ADD CONSTRAINT topo_grimpeur_topo_fk
-FOREIGN KEY (topo_id)
+FOREIGN KEY (topo_commente_id)
 REFERENCES public.topo (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
@@ -241,13 +222,13 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 ALTER TABLE public.secteur ADD CONSTRAINT site_secteur_fk
-FOREIGN KEY (id_1)
+FOREIGN KEY (site_id)
 REFERENCES public.site (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.topo_site_descripteur ADD CONSTRAINT site_topo_site_fk
+ALTER TABLE public.topo_site_descipteur ADD CONSTRAINT site_topo_site_fk
 FOREIGN KEY (site_id)
 REFERENCES public.site (id)
 ON DELETE NO ACTION
@@ -255,14 +236,7 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 ALTER TABLE public.commentaire_site ADD CONSTRAINT site_commentaire_site_fk
-FOREIGN KEY (site_id)
-REFERENCES public.site (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
-ALTER TABLE public.grimpeur_site_createur ADD CONSTRAINT site_grimpeur_site_ajout_fk
-FOREIGN KEY (site_id)
+FOREIGN KEY (site_commente_id)
 REFERENCES public.site (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
